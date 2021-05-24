2dc1e7c9fc133557e40052a3294416f27fd6cce0
Josh B <4ctrl.alt.del@gmail.com>
2021-05-24 15:43:09 -0700

    Added base class.
    Added tripal job factory base class. After lots of trial and error that became
    rather simple since the base class should not implement any of the three
    interface methods.

---
+++ tripal/src/Plugin/TripalJob/TripalJobFactoryBase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\tripal\Plugin\TripalJob;
+
+use Drupal\tripal\Plugin\TripalJob\TripalJobFactoryInterface;
+use Drupal\Component\Plugin\PluginBase;
+
+/**
+ * Base class for Tripal Job Factory plugins.
+ */
+interface TripalJobFactoryBase extends PluginBase implements TripalJobFactoryInterface {
+}
