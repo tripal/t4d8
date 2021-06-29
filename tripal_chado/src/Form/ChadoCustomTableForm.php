991368a7e0bfbcf9e96f64450106b9883b49e359
Risharde <risharde@gmail.com>
2021-06-28 22:27:12 -0400

    New tripal_chado.views.inc file that adds custom tables view settings within the Views module system

--- tripal_chado/src/Form/ChadoCustomTableForm.php
+++ tripal_chado/src/Form/ChadoCustomTableForm.php
@@ -294,64 +294,6 @@ class ChadoCustomTableForm extends FormBase {
     drupal_goto("admin/tripal/storage/chado/custom_tables");
   }
   
-  /**
-   * Just a simple form for confirming deletion of a custom table
-   *
-   */
-  function tripal_custom_tables_delete_form($form, &$form_state, $table_id) {
-  
-    // get details about this table entry
-    $sql = "SELECT * FROM {tripal_custom_tables} WHERE table_id = :table_id";
-    $results = db_query($sql, [':table_id' => $table_id]);
-    $entry = $results->fetchObject();
-  
-    // if this is a materialized view then don't allow editing with this function
-    if ($entry->mview_id) {
-      drupal_set_message("This custom table is a materialized view. Please use the " . Link::fromTextAndUrl('Materialized View', Url::fromUserInput('admin/tripal/storage/chado/mviews')) . " interface to delete it.", 'error');
-      drupal_goto("admin/tripal/storage/chado/custom_tables");
-      return [];
-    }
-  
-  
-    $form = [];
-    $form['table_id'] = [
-      '#type' => 'value',
-      '#value' => $table_id,
-    ];
-  
-    $form['sure'] = [
-      '#type' => 'markup',
-      '#markup' => '<p>Are you sure you want to delete the "' . $entry->table_name . '" custom table?</p>',
-    ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => 'Delete',
-    ];
-    $form['cancel'] = [
-      '#type' => 'submit',
-      '#value' => 'Cancel',
-    ];
-    return $form;
-  }
-  
-  /**
-   * form submit hook for the tripal_custom_tables_delete_form form.
-   *
-   * @param $form
-   * @param $form_state
-   */
-  function tripal_custom_tables_delete_form_submit($form, &$form_state) {
-    $action = $form_state['clicked_button']['#value'];
-    $table_id = $form_state['values']['table_id'];
-  
-    if (strcmp($action, 'Delete') == 0) {
-      chado_delete_custom_table($table_id);
-    }
-    else {
-      drupal_set_message(t("No action performed."));
-    }
-    drupal_goto("admin/tripal/storage/chado/custom_tables");
-  }  
 }
 
 
